fd3f6ed773a488bd58f557323877e1cfc4f7b968
Josh Cartmell <joshcartme@gmail.com>
2012-11-08 14:49:35 -0800

    Created migrations which copy data from the old site ForeignKey to the new sites m2m.

---
+++ mezzanine/conf/migrations/0005_del_field_setting_site.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding M2M table for field sites on 'Setting'
+        db.create_table('conf_setting_sites', (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('setting', models.ForeignKey(orm['conf.setting'], null=False)),
+            ('site', models.ForeignKey(orm['sites.site'], null=False))
+        ))
+        db.create_unique('conf_setting_sites', ['setting_id', 'site_id'])
+
+        # copy ForeignKey to m2m
+        if not db.dry_run:
+            for set in orm.Setting.objects.all():
+                set.sites.add(set.site)
+                set.save()
+
+        # Deleting field 'Setting.site'
+        db.delete_column('conf_setting', 'site_id')
+
+    def backwards(self, orm):
+        # Adding field 'Setting.site'
+        db.add_column('conf_setting', 'site',
+                      self.gf('django.db.models.fields.related.ForeignKey')(default=1, to=orm['sites.Site']),
+                      keep_default=False)
+
+        # Removing M2M table for field sites on 'Setting'
+        db.delete_table('conf_setting_sites')
+
+
+    models = {
+        'conf.setting': {
+            'Meta': {'object_name': 'Setting'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'site': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['sites.Site']"}),
+            'sites': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['sites.Site']", 'symmetrical': 'False'}),
+            'value': ('django.db.models.fields.CharField', [], {'max_length': '2000'})
+        },
+        'sites.site': {
+            'Meta': {'ordering': "('domain',)", 'object_name': 'Site', 'db_table': "'django_site'"},
+            'domain': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        }
+    }
+
+    complete_apps = ['conf']
